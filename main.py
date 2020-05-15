f3e96b700923ede8b95e6bf581e3f28237a7edd7
Alex muraru <alexandru.muraru@student.tuiasi.ro>
2020-05-15 12:42:39 +0300

    Imported camera module from Miguel

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request,Response
 from flask import render_template
 from threading import Thread
 import json
 import RPi.GPIO as GPIO
 import time
+from camera_pi import Camera
 
 #18 trig distance sensor
 trig=18
@@ -196,7 +197,6 @@ app=Flask(__name__)
 masina=Car()
 
 
-
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -228,6 +228,16 @@ def moveTank():
 	masina.moveTankCoords(x,y)
 	return "ok"
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
 	try:
